4dc1533b291114f7522f63df8da2deb357fbbb4d
baoyong <545009168@qq.com>
2018-02-12 15:29:52 +0800

    'by'

--- src/apl/index.js
+++ src/apl/index.js
@@ -2,3 +2,5 @@ import fetch from '../utils/fetch';
 
 export const getSidebarDate = () => fetch('/sidebar/getSidebarData', {}, 'GET');
 export const removeSidebarDate = (title) => fetch('/sidebar/removeSidebarData', {title}, 'POST');
+export const addSidebarDate = (newSidebarData) => fetch('/sidebar/addSidebarData', {newSidebarData}, 'POST');
+export const updateSidebarData = (updateData) => fetch('/sidebar/updateSidebarData', {updateData}, 'POST');
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Table.css';
-import { getSidebarDate,removeSidebarDate } from '../apl';
+import { getSidebarDate,removeSidebarDate,addSidebarDate,updateSidebarData } from '../apl';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
 import Dialog from 'material-ui/Dialog';
@@ -20,17 +20,54 @@ class MyTable extends React.Component {
         super(props);
         this.state = {
             sidebarList: [],
+            editItem: {
+                title: 'child',
+                children: [
+                    {
+                        title: 'index',
+                        url:'hhj',
+                        as: 'ghgjh',
+                    }
+                ]
+            },
             open: false,
-            openAddDialog: true,
+            openAddDialog: false,
+            openEditDialog: false,
+            value: 'Property Value',
+            fatherTitle: '',
+            sonItemList: [
+                {
+                    title: '',
+                    url: '',
+                    as: '',
+                },{
+                    title: '',
+                    url: '',
+                    as: '',
+                }
+            ]
         };
         this.showAddDialog = this.showAddDialog.bind(this);
         this.hideAddDialog = this.hideAddDialog.bind(this);
+        this.hideAddDialog2 = this.hideAddDialog2.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChange2 = this.handleChange2.bind(this);
+        this.handleChangeFather = this.handleChangeFather.bind(this);
+        this.addSonItem = this.addSonItem.bind(this);
+        this.delSonItem = this.delSonItem.bind(this);
+        this.submit = this.submit.bind(this);
+        this.submit2 = this.submit2.bind(this);
+        this.editData = this.editData.bind(this);
     }
 
     removeData(title){
         console.log('title = ' + title);
         removeSidebarDate(title).then(res => {
-            console.log(res);
+            getSidebarDate().then(res => {
+                this.setState({
+                    sidebarList: res
+                })
+            })
         })
     }
 
@@ -45,6 +82,108 @@ class MyTable extends React.Component {
             openAddDialog: false
         });
     };
+
+    hideAddDialog2 (){
+        this.setState({
+            openEditDialog: false
+        });
+    };
+
+    handleChange(event, index, type){
+        let newValue = event.target.value;
+        this.setState({
+            sonItemList: this.state.sonItemList.map((item, i) => {
+                if(i === index){
+                    item[type] = newValue;
+                    return item;
+                }
+                return item;
+            })
+        })
+    }
+
+    handleChange2(event, index, type){
+        let newValue = event.target.value;
+        this.setState({
+            editItem: {
+                ...this.state.editItem,
+                children: this.state.editItem.children.map((item, i) => {
+                    if(i === index){
+                        item[type] = newValue;
+                        return item;
+                    }
+                    return item;
+                })
+            }
+        })
+    }
+
+    handleChangeFather(event){
+        this.setState({
+            fatherTitle: event.target.value,
+        });
+    }
+
+    addSonItem(){
+        let obj = {
+            title: '',
+            url: '',
+            as: '',
+        };
+        this.setState({
+            sonItemList: [...this.state.sonItemList, obj]
+        })
+    }
+
+    delSonItem(index){
+        this.setState({
+            sonItemList: this.state.sonItemList.filter((item, i) => {
+                if(index === i){
+                    return false;
+                }
+                return true;
+            })
+        })
+    }
+
+    submit(){
+        let obj = Object.assign({}, {title: this.state.fatherTitle}, {children: this.state.sonItemList});
+        console.log(obj);
+        console.log(JSON.stringify(obj));
+        addSidebarDate(JSON.stringify(obj)).then(res => {
+            this.setState({
+                openAddDialog: false
+            });
+            getSidebarDate().then(res => {
+                this.setState({
+                    sidebarList: res
+                })
+            })
+        })
+    }
+
+    submit2(){
+        console.log(this.state.editItem);
+        updateSidebarData(JSON.stringify(this.state.editItem)).then(res => {
+            this.setState({
+                openEditDialog: false
+            });
+            getSidebarDate().then(res => {
+                this.setState({
+                    sidebarList: res
+                })
+            })
+        })
+    }
+
+    editData(index){
+        this.setState({
+            editItem: this.state.sidebarList[index]
+        })
+        this.setState({
+            openEditDialog: true
+        });
+    }
 
     componentDidMount(){
         getSidebarDate().then(res => {
@@ -66,7 +205,20 @@ class MyTable extends React.Component {
                 label="Submit"
                 primary={true}
                 keyboardFocused={true}
-                onClick={this.hideAddDialog}
+                onClick={this.submit}
+            />,
+        ];
+        const actions2 = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onClick={this.hideAddDialog2}
+            />,
+            <FlatButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onClick={this.submit2}
             />,
         ];
         return (
@@ -91,7 +243,7 @@ class MyTable extends React.Component {
                                     <TableRowColumn>{item.title}</TableRowColumn>
                                     <TableRowColumn>{item.children.length}</TableRowColumn>
                                     <TableRowColumn className='handle'>
-                                        <span className="edit">编辑</span>
+                                        <span className="edit" onClick={() => this.editData(index)}>编辑</span>
                                         <span> | </span>
                                         <span className="delete" onClick={() => {this.removeData(item.title)}}>删除</span>
                                     </TableRowColumn>
@@ -119,34 +271,108 @@ class MyTable extends React.Component {
                         <span>主标题: </span>
                         <TextField
                             hintText="Hint Text"
+                            value={this.state.fatherTitle}
+                            onChange={(event) => {this.handleChangeFather(event)}}
                         /><br />
                     </div>
-                    <div className="son">
-                        <div className="item">
-                            <span>子标题: </span>
-                            <TextField
-                                className='TextField'
-                                hintText="Hint Text"
-                                style={{width: '70%'}}
-                                value="hello"
-                            />
-                        </div>
-                        <div className="item">
-                            <span>url: </span>
-                            <TextField
-                                className='TextField'
-                                hintText="Hint Text"
-                                style={{width: '70%'}}
-                            />
-                        </div>
-                        <div className="item">
-                            <span>as: </span>
-                            <TextField
-                                className='TextField'
-                                hintText="Hint Text"
-                                style={{width: '70%'}}
-                            />
-                        </div>
+
+                    {
+                        this.state.sonItemList.map((item, index) => (
+                            <div className="son" key={index}>
+                                <div className="item">
+                                    <span>子标题: </span>
+                                    <TextField
+                                        className='TextField'
+                                        hintText="Hint Text"
+                                        style={{width: '70%'}}
+                                        value={this.state.sonItemList[index].title}
+                                        onChange={(event) => {this.handleChange(event, index, 'title')}}
+                                    />
+                                </div>
+                                <div className="item">
+                                    <span>url: </span>
+                                    <TextField
+                                        className='TextField'
+                                        hintText="Hint Text"
+                                        style={{width: '70%'}}
+                                        value={this.state.sonItemList[index].url}
+                                        onChange={(event) => {this.handleChange(event, index, 'url')}}
+                                    />
+                                </div>
+                                <div className="item">
+                                    <span>as: </span>
+                                    <TextField
+                                        className='TextField'
+                                        hintText="Hint Text"
+                                        style={{width: '70%'}}
+                                        value={this.state.sonItemList[index].as}
+                                        onChange={(event) => {this.handleChange(event, index, 'as')}}
+                                    />
+                                </div>
+                                <div className="delete" onClick={() => {this.delSonItem(index)}}>删除</div>
+                            </div>
+                        ))
+                    }
+                    <div className="btnBox">
+                        <RaisedButton label="添加" primary={true} className="addBtn" onClick={this.addSonItem}/>
+                    </div>
+                </Dialog>
+                {/* 编辑的dialog */}
+                <Dialog
+                    title="修改列表"
+                    actions={actions2}
+                    modal={false}
+                    open={this.state.openEditDialog}
+                    onRequestClose={this.handleClose}
+                >
+                    <div className="father">
+                        <span>主标题: </span>
+                        <TextField
+                            hintText="Hint Text"
+                            disabled={true}
+                            value={this.state.editItem.title}
+                        /><br />
+                    </div>
+
+                    {
+                        this.state.editItem.children.map((item, index) => (
+                            <div className="son" key={index}>
+                                <div className="item">
+                                    <span>子标题: </span>
+                                    <TextField
+                                        className='TextField'
+                                        hintText="Hint Text"
+                                        style={{width: '70%'}}
+                                        value={item.title}
+                                        onChange={(event) => {this.handleChange2(event, index, 'title')}}
+                                    />
+                                </div>
+                                <div className="item">
+                                    <span>url: </span>
+                                    <TextField
+                                        className='TextField'
+                                        hintText="Hint Text"
+                                        style={{width: '70%'}}
+                                        value={item.url}
+                                        onChange={(event) => {this.handleChange2(event, index, 'url')}}
+                                    />
+                                </div>
+                                <div className="item">
+                                    <span>as: </span>
+                                    <TextField
+                                        className='TextField'
+                                        hintText="Hint Text"
+                                        style={{width: '70%'}}
+                                        value={item.as}
+                                        onChange={(event) => {this.handleChange2(event, index, 'as')}}
+                                    />
+                                </div>
+                                <div className="delete" onClick={() => {this.delSonItem(index)}}>删除</div>
+                            </div>
+                        ))
+                    }
+                    <div className="btnBox">
+                        <RaisedButton label="添加" primary={true} className="addBtn" onClick={this.addSonItem}/>
                     </div>
                 </Dialog>
             </div>
